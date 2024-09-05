a5b28e38f8f299fdb7049bf41db549554200ad29
Ezra Williams <ewilliams@perscholas.org>
2024-09-05 15:30:49 -0400

    Code we worked on 9/5/2024

---
+++ Module303/src/main/java/org/ezrawilliams/introtojavabasics/WelcomeToJava.java
@@ -0,0 +1,35 @@
+package org.ezrawilliams.introtojavabasics;
+/*
+Classes in Java should be in UpperCamelCase
+Everything that belongs to the class goes inside of the curly braces
+Our class is public meaning it is accessible to any other classes
+This is a MULTI-LINE COMMENT - it spans lines
+What is a comment - just a note for me or for you
+Curly braces - every BLOCK of code is inside of opening and closing
+curly braces
+ */
+
+/**
+ * JAVADOCS - special comments - they create a special document
+ * that includes whatever you write in here.
+ */
+
+public class WelcomeToJava {
+    /*
+    The main method is the entry point into our application
+    Public - accessible to any outside classes
+    static - belongs DIRECTLY to the WelcomeToJava class
+    void - does not RETURN a value
+    In parentheses we see what the method takes in
+     */
+    public static void main(String[] args) {
+        //When we come back we will learn ONE LINE comments like this
+        /*
+        In Java, we use the System class print methods (println (print line))
+        to display things to the screen or console.
+         */
+        System.out.println("Welcome to Java!!!!!!!!!!!! Yayyyy!!!");
+        System.out.println("And we can write things to the screen!");
+        System.out.println("We will still write the whole thing from scratch");
+    }
+}
