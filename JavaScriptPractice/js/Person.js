0535cad06028f58ed96894ab70f4948a2ef57f7a
Ezra Williams <ewilliams@perscholas.org>
2024-11-15 14:27:48 -0500

    11/15/2024 - advanced JavaScript

--- JavaScriptPractice/js/Person.js
+++ JavaScriptPractice/js/Person.js
@@ -1,28 +1,43 @@
 // Class Declaration
 class Person {
   static species = "Human";
+  #firstName; #lastName; #years; #eyeColor;
 
   constructor(firstName, lastName, age, eyeColor) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.years = age;
-    this.eyeColor = eyeColor;
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#years = age;
+    this.#eyeColor = eyeColor;
   }
   get name() {
-    return this.firstName + " " + this.lastName;
+    return this.#firstName + " " + this.#lastName;
   }
-  set age(new_age) { this.years = new_age; }
-  get age() { return this.years; }
-
+  set age(new_age) { this.#years = new_age; }
+  get age() { return this.#years; }
   static greet() {
-    // Math.random() is also a static method!
     if (Math.random() <= 0.5)
       console.log("Hello!");
     else
       console.log("Hi!");
   }
 };
-const myFather = new Person("John", "Doe", 50, "blue");
+
+class Learner extends Person{
+  #grades = [];
+  set grade(new_grade) {
+    this.#grades.push(new_grade);
+  }
+  get average() {
+    let total = 0;
+    for (let i = 0; i < this.#grades.length; i++) {
+      total += this.#grades[i];
+    }
+    return Math.round(total/this.#grades.length);
+  }
+
+};
+
+/*const myFather = new Person("John", "Doe", 50, "blue");
 console.log(myFather.firstName);	// John
 console.log(myFather.lastName);	// Doe
 console.log(myFather.name);	// John Doe
@@ -31,4 +46,12 @@ console.log(myFather.age);		// 51
 
 console.log(myFather.species);	// undefined
 console.log(Person.species);	// Human
-Person.greet();
+Person.greet();*/
+
+const myFather = new Learner("John", "Doe", 50, "blue");
+myFather.grade = 100; // Note how these statements do
+myFather.grade = 85;  // not change a property called
+myFather.grade = 96;  // called “grade”; instead,
+// they call our setter method
+console.log(myFather.average);	// 94
+console.log(myFather.name);	// John Doe
